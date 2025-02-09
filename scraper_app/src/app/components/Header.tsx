963887a27bec0c3801cc4dedf06800bcac5f1750
Gitguruguru <superandrii1312@gmail.com>
2025-02-09 18:20:04 +0100

    remove outdated files add 2 panter

---
+++ scraper_app/src/app/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className=' w-screen h-10 flex flex-row'>
+        <div className='' id='Logo'>
+          
+        </div>
+        <div className="" id='Nav_bar'>
+            <nav>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Pricing</a>
+            </nav>
+        </div>
+        <div className=''></div>
+    </div>
+  )
+}
